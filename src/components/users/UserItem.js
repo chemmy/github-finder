e91816ec7e8932740830e9724bc60de26a830aaf
Chemmy Tutor <chemmy0430@gmail.com>
2019-11-12 21:03:58 +0800

    tgf-0002: Components, States, and Props; both class-based and functional; api calls and env file

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { PropTypes } from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_url }}) => {
+  return (
+    <div className="card text-center">
+      <img 
+        className='round-img'
+        src={ avatar_url } alt='' 
+        style={{ width: '60px' }}
+      />
+      <h3>{ login }</h3>
 
-    return (
-      <div className="card text-center">
-        <img 
-          className='round-img'
-          src={ avatar_url } alt='' 
-          style={{ width: '60px' }}
-        />
-        <h3>{ login }</h3>
-
-        <div>
-          <a 
-            href={ html_url } 
-            className="btn btn-dark btn-sm my-1"
-          >More
-          </a>
-        </div>
+      <div>
+        <a 
+          href={ html_url } 
+          className="btn btn-dark btn-sm my-1"
+        >More
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
