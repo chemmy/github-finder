fd6e4dd5c70fc07cf89c7fe86f62de082480228e
Chemmy Tutor <chemmy0430@gmail.com>
2019-11-17 13:34:45 +0800

    tgf-0003: Events, passing props, react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Search from './components/users/Search';
 import Users from './components/users/Users';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -8,6 +12,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   async componentDidMount() {
@@ -20,20 +25,55 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [] });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }});
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
+    const { users, loading, alert} = this.state;
     return (
-      <div className="App">
-        <Navbar 
-          title='Github Finder'
-          icon='fab fa-github'
-        />
-        <div className="container">
-          <Users 
-            loading={ this.state.loading }
-            users={ this.state.users}
+      <Router>
+        <div className="App">
+          <Navbar 
+            title='Github Finder'
+            icon='fab fa-github'
           />
+          <div className="container">
+            <Alert alert={alert}/>
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users 
+                    loading={ loading }
+                    users={ users}
+                  />
+                </Fragment>
+              )}></Route>
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p>Search github users</p>
+    </div>
+  )
+}
+
+export default About;
